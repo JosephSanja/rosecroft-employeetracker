88561688e02e718d5ebf9920b2f25e3ceb60bedc
JosephSsanja <joevega.sanju6ss@gmail.com>
2024-02-15 15:38:48 +0000

    j

--- src/index.js
+++ src/index.js
@@ -6,16 +6,7 @@ import reportWebVitals from './reportWebVitals';
 
 import { ToastContainer } from 'react-toastify';
 
-const jsonSever = require("json-server");//importing json-server library
-const server = jsonSever.create();
-const router = json.router("db.json");
-const middlewares = jsonSever.defaults();
-const port = process.env.PORT || 8000; // chose port from here like 8000,3001
 
-server.use(middlewares);
-server.use(router);
-
-server.listen(port);
 
 
 
