39648c5bf47d5b04280c9681144399f435710dc3
JosephSsanja <joevega.sanju6ss@gmail.com>
2024-02-16 17:58:49 +0000

    live

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import Login from './Login/Login';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter as Router} from 'react-router-dom';
+import {BrowserRouter as Routes} from 'react-router-dom';
 
 import { ToastContainer } from 'react-toastify';
 
@@ -15,9 +15,12 @@ import { ToastContainer } from 'react-toastify';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-     <Router>
+    <BrowserRouter>
+    <Routes>
      <Login /> 
-    </Router>
+    </Routes>
+    </BrowserRouter>
+     
     
    
    <ToastContainer/>
