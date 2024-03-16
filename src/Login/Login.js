c0d7fd1d93df1d0aa33972b2a120bb8fa3ef66ad
JosephSsanja <joevega.sanju6ss@gmail.com>
2024-03-16 15:16:51 +0000

    ms

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -37,7 +37,7 @@ const Login=() => {
               toast.success('Success');
               sessionStorage.setItem('username',username);
               sessionStorage.setItem('userrole',resp.role);
-              usenavigate('/ShiftScheduler')
+              usenavigate('/')
               
               
             }else{
